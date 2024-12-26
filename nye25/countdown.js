5333ca122d7bcea6c53f49da5ffa23e06c519f70
Faulkner Bodbyl-Mast <faulkner.blue@gmail.com>
2024-12-26 13:26:04 -0500

    6pm drop change

--- nye25/countdown.js
+++ nye25/countdown.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Dec 31, 2024 12:00:00 EST").getTime();
+var countDownDate = new Date("Dec 31, 2024 18:00:00 EST").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
